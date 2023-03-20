062f1360a4c476e727c413ccaa974f5a5c875f64
USER <malindu@web3genes.com>
2023-03-20 21:55:53 +0530

    third commit

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/cicd-frontend/deploy.log
 
 echo 'pm2 restart frontend' >> /home/ec2-user/cicd-frontend/deploy.log
-pm2 restart frontend >> /home/ec2-user/cicd-frontend/deploy.log
+pm2 restart cicd-frontend >> /home/ec2-user/cicd-frontend/deploy.log
