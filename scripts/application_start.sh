c63840448be086a02347ef7d5b67ae42f41266e2
USER <malindu@web3genes.com>
2023-03-20 22:01:41 +0530

    fourth commit

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,5 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/cicd-frontend/deploy.log
 
 echo 'pm2 restart frontend' >> /home/ec2-user/cicd-frontend/deploy.log
-pm2 restart cicd-frontend >> /home/ec2-user/cicd-frontend/deploy.log
+sudo pm2 restart cicd-frontend >> /home/ec2-user/cicd-frontend/deploy.log
+sudo pm2 save >> /home/ec2-user/cicd-frontend/deploy.log
