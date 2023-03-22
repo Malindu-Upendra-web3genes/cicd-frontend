78ea3f05655bf2e63f5f36378291ddd5f335a529
USER <malindu@web3genes.com>
2023-03-22 09:12:44 +0530

    added node.js yml and changed app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing CICD with react
+          asdfghjklqwertyuiop
         </p>
         <a
           className="App-link"
