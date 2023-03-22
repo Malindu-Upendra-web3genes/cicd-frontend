ff5033a4cca4ea3cc47d525658d66b584146a8c4
USER <malindu@web3genes.com>
2023-03-22 09:40:10 +0530

    changed app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          changed again changed again changed again changed again changed again
+          changed again changed again
         </p>
         <a
           className="App-link"
