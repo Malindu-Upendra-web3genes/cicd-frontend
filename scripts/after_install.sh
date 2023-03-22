77179ba7fc72103a768afdeb1e2d46755fa3751b
USER <malindu@web3genes.com>
2023-03-22 08:54:33 +0530

    changed script files

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/cicd-frontend/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/buyasia/cicd-frontend/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/cicd-frontend/deploy.log
-cd /home/ec2-user/cicd-frontend >> /home/ec2-user/cicd-frontend/deploy.log
+echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/buyasia/cicd-frontend/deploy.log
+cd /home/ec2-user/buyasia/cicd-frontend >> /home/ec2-user/buyasia/cicd-frontend/deploy.log
 
-echo 'npm install' >> /home/ec2-user/cicd-frontend/deploy.log 
-npm i --legacy-peer-deps >> /home/ec2-user/cicd-frontend/deploy.log
+echo 'npm install' >> /home/ec2-user/buyasia/cicd-frontend/deploy.log 
+npm i --legacy-peer-deps >> /home/ec2-user/buyasia/cicd-frontend/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/cicd-frontend/deploy.log
+echo 'run application_start.sh: ' >> /home/ec2-user/buyasia/cicd-frontend/deploy.log
 
-echo 'pm2 restart frontend' >> /home/ec2-user/cicd-frontend/deploy.log
-sudo pm2 restart cicd-frontend >> /home/ec2-user/cicd-frontend/deploy.log
-sudo pm2 save >> /home/ec2-user/cicd-frontend/deploy.log
+echo 'pm2 restart frontend' >> /home/ec2-user/buyasia/cicd-frontend/deploy.log
+sudo pm2 restart cicd-frontend >> /home/ec2-user/buyasia/cicd-frontend/deploy.log
+sudo pm2 save >> /home/ec2-user/buyasia/cicd-frontend/deploy.log
